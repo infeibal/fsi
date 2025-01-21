89e1b9edc8a594125f0bf7994bd007f13ac1fa42
infeibal <theliamegames@gmail.com>
2025-01-21 11:27:36 +0700

    chore: remote strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Application } from "./common/application/application";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Application />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<Application />);
