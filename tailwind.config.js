6e94affccd645b787476f3487c0428912ae8b6bd
infeibal <theliamegames@gmail.com>
2025-02-01 23:29:03 +0700

    chore: remove unused font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        oswald: ["Oswald", "sans-serif"],
-      },
       colors: {
         system: {
           purple: "#7e47be",
