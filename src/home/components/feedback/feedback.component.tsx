70968fc1d4395f10f74f1c78b729b1ea3bb60813
infeibal <theliamegames@gmail.com>
2025-02-03 18:54:51 +0700

    fix(feedback): change margin size

--- src/home/components/feedback/feedback.component.tsx
+++ src/home/components/feedback/feedback.component.tsx
@@ -2,7 +2,7 @@ import { FeedBackList } from "./feedback_list.components";
 
 export const HomeFeedback = () => {
   return (
-    <section className="py-20">
+    <section className="pt-20">
       <h1 className="text-4xl px-10 mb-16 font-semibold">
         Что говорят наши клиенты:
       </h1>
