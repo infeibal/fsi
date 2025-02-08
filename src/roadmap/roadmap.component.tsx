31763b92d4ce8bae1f4a3b09a0fc1c37c53424ac
infeibal <theliamegames@gmail.com>
2025-02-08 19:49:04 +0700

    feat(roadmap): create roadmap component

---
+++ src/roadmap/roadmap.component.tsx
@@ -0,0 +1,10 @@
+export const Roadmap = () => {
+  return (
+    <div className="mx-auto w-fit">
+      <img
+        src="https://static.wixstatic.com/media/ba338e_61ab7789b2744a078b87af968a59c48b~mv2.jpg/v1/fill/w_924,h_521,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/photo_2024-12-12_21-42-20.jpg"
+        alt="roadmap_img"
+      />
+    </div>
+  );
+};
