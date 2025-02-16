70f78aa7379fffee6bfdc98992c55c25ab29e293
infeibal <theliamegames@gmail.com>
2025-02-16 22:16:45 +0700

    feat(module): add title changing when link was changed

--- src/home/home-layout.tsx
+++ src/home/home-layout.tsx
@@ -1,8 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { HomeFooter } from "./components/home-footer.component";
 import { HomeHeader } from "./components/header/home-header.component";
+import { useEffect } from "react";
 
 export const HomeLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/roadmap":
+        document.title = "РОАДМАПА | ФСИ Банк";
+        break;
+
+      case "/converter":
+        document.title = "Конвертер | ФСИ Банк";
+        break;
+
+      case "/making":
+        document.title = "Оформление карты | ФСИ Банк";
+        break;
+
+      case "/secret":
+        document.title = "Гэрри Уткорот | ФСИ Банк";
+        break;
+
+      default:
+        document.title = "ФСИ Банк";
+    }
+  }, [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <HomeHeader />
